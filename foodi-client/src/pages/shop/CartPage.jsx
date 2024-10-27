e42cf44d0d20e499f61857446a3ad327f1bdf002
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-10-27 23:34:28 +0530

    Delete function for cart

--- foodi-client/src/pages/shop/CartPage.jsx
+++ foodi-client/src/pages/shop/CartPage.jsx
@@ -1,6 +1,39 @@
 import React from "react";
-
+import useCart from "../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
 const CartPage = () => {
+  const [cart, refetch] = useCart();
+
+  //handleDelete button
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/carts/${item._id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your item has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
   return (
     <div className="section-container ">
       {/* banner */}
@@ -32,36 +65,31 @@ const CartPage = () => {
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <td>1</td>
-                <td>
-                  <div className="flex items-center gap-3">
-                    <div className="avatar">
-                      <div className="mask mask-squircle h-12 w-12">
-                        <img
-                          src="https://img.daisyui.com/images/profile/demo/2@94.webp"
-                          alt="Avatar Tailwind CSS Component"
-                        />
+              {cart.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img src={item.image} alt="" />
+                        </div>
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
-                <th>
-                  <button className="btn btn-ghost btn-xs">details</button>
-                </th>
-              </tr>
+                  </td>
+                  <td className="font-medium">{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price}</td>
+                  <th>
+                    <button
+                      className="btn btn-ghost text-red btn-xs"
+                      onClick={() => handleDelete(item)}
+                    >
+                      <FaTrash />
+                    </button>
+                  </th>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
--- foodi-server/Index.js
+++ foodi-server/Index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // mongodb config
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodi-cluster.gcbr7.mongodb.net/?retryWrites=true&w=majority&appName=foodi-cluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -53,6 +53,22 @@ async function run() {
       const email = req.query.email;
       const filter = { email: email };
       const result = await cartCollections.find(filter).toArray();
+      res.send(result);
+    });
+
+    // get specific cart
+    app.get("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const result = await cartCollections.findOne(filter);
+      res.send(result);
+    });
+
+    // delete items from cart
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const result = await cartCollections.deleteOne(filter);
       res.send(result);
     });
 
